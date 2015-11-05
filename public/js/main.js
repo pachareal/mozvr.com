e845f87059cd1b17748e405267825ebaaf3c4437
caseyyee <caseyyee.ca@gmail.com>
2015-11-05 13:40:06 -0800

    mozvr.com site stubbed out

--- public/js/main.js
+++ public/js/main.js
@@ -15,39 +15,51 @@
   // NOTE: Not using classList for IE compatibility.
   document.body.className += 'ontouchstart' in window ? ' has-touch' : ' lacks-touch';
 
+  function resizeHandler(e) {
+    $$('.size-to-window').forEach(function(el) {
+      el.style.width = window.innerWidth + 'px';
+      el.style.height = window.innerHeight + 'px';
+    });
+  }
+
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
+
+  document.body.className += navigator.getVRDevices ? ' vr' : ' novr';
+
   // Google Analytics.
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-49796218-16', 'auto');
-  ga('send', 'pageview');
+  // ga('create', 'UA-49796218-16', 'auto');
+  // ga('send', 'pageview');
 
-  $$('.highlight__item').forEach(function (el) {
-    el.addEventListener('click', function () {
-      ga('send', 'event', 'click.demo',
-         el.querySelector('.highlight__item__title').textContent);
-    });
-  });
+  // $$('.highlight__item').forEach(function (el) {
+  //   el.addEventListener('click', function () {
+  //     ga('send', 'event', 'click.demo',
+  //        el.querySelector('.highlight__item__title').textContent);
+  //   });
+  // });
 
-  $$('#mozilla a').forEach(function (el) {
-    el.addEventListener('click', function () {
-      ga('send', 'event', 'click.footer', el.textContent);
-    });
-  });
+  // $$('#mozilla a').forEach(function (el) {
+  //   el.addEventListener('click', function () {
+  //     ga('send', 'event', 'click.footer', el.textContent);
+  //   });
+  // });
 
 
   // Outsmart spam bots.
-  var pressEmail = $('.footer__press');
+  // var pressEmail = $('.footer__press');
 
-  function setPressEmail() {
-    pressEmail.href = 'mailto:press';
-    pressEmail.href += '@mozilla.com';
-  }
+  // function setPressEmail() {
+  //   pressEmail.href = 'mailto:press';
+  //   pressEmail.href += '@mozilla.com';
+  // }
 
-  pressEmail.addEventListener('click', setPressEmail);
-  $('#mozilla').addEventListener('mouseover', setPressEmail);
+  // pressEmail.addEventListener('click', setPressEmail);
+  // $('#mozilla').addEventListener('mouseover', setPressEmail);
 
 
   // Open external links in new tabs.
